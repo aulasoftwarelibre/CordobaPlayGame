84edea4aac523b7ed59372d42a86f58ad1c94acf
darkghost078 <147414430+darkghost078@users.noreply.github.com>
2025-03-13 18:33:03 +0100

    Initial commit

---
+++ progress_bar.gd
@@ -0,0 +1,8 @@
+class_name BarraProgreso
+extends ProgressBar
+
+func _on_timer_timeout() -> void:
+	value-= 1
+	print("Esta bajando")
+	if value<=0:
+		get_tree().change_scene_to_file("res://GameOver.tscn")
