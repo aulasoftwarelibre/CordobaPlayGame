307099d84afdaec79dfcd580562ed9fcdf2b373f
darkghost078 <daviddarkghost@gmail.com>
2025-03-14 09:45:34 +0100

    fix: Added a Win Screen if the bar reach the 100%

--- GameControl.gd
+++ GameControl.gd
@@ -1,1 +1,9 @@
 extends Control
+@onready var Barra=$HBoxContainer/Progreso/ColorRect/MarginContainer/ProgressBar
+
+func _process(delta):
+	if Input.is_action_pressed("Pescar"):
+		Barra.value+=0.35;
+	if Barra.value>=100:
+		get_tree().change_scene_to_file("res://WinScreen.tscn")
+		
