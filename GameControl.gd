a66eeb6d8a7468d15982019d4db9c1b160217a4d
David Martínez <daviddarkghost@gmail.com>
2025-03-24 18:19:12 +0100

    Added the catch Bar

--- GameControl.gd
+++ GameControl.gd
@@ -2,8 +2,6 @@ extends Control
 @onready var Barra=$HBoxContainer/Progreso/ColorRect/MarginContainer/ProgressBar
 
 func _process(delta):
-	if Input.is_action_pressed("Pescar"):
-		Barra.value+=0.35;
 	if Barra.value>=100:
 		get_tree().change_scene_to_file("res://WinScreen.tscn")
 		
--- caja.gd
+++ caja.gd
@@ -2,6 +2,14 @@ extends Control
 @onready var temp=$MovPez
 @onready var yMax=int($ColorRect/MarginContainer/ColorRect.size.y)
 @onready var pez=$ColorRect/MarginContainer/ColorRect/TextureRect
+@onready var catch=$ColorRect/MarginContainer/ColorRect/Catch
+
+@export var catchPeso: float =100
+@export var tMin: float =0.6
+@export var tMax: float =5.0
+@export var pezVel: float =0.6
+@export var catchLevantar: float =250
+
 var posSalto: int =0
 
 func _ready() -> void:
@@ -10,7 +18,13 @@ func _ready() -> void:
 	nuevoSalto()
 
 func _process(delta: float) -> void:
-	pez.position.y= lerpf(pez.position.y,posSalto,0.6)
+	pez.position.y= lerpf(pez.position.y,posSalto,pezVel)
+	
+	if Input.is_action_pressed("Pescar"):
+		catch.position.y-=catchLevantar*delta
+	if catch.position.y+catch.size.y<=yMax:
+		catch.position.y+=catchPeso*delta
+
 
 func _on_mov_pez_timeout() -> void:
 	nuevoSalto()
@@ -18,5 +32,4 @@ func _on_mov_pez_timeout() -> void:
 func nuevoSalto() -> void:
 	posSalto= randi()%yMax
 	print("Salto", posSalto)
-	temp.wait_time=randf_range(0.6,5)
-	
+	temp.wait_time=randf_range(tMin,tMax)
