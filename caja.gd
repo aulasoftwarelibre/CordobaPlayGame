65ea3abbd5a42199ea02f0e60ac861b1ee983273
David Martínez <daviddarkghost@gmail.com>
2025-03-24 19:52:46 +0100

    There is a game XD

--- caja.gd
+++ caja.gd
@@ -1,16 +1,19 @@
 extends Control
-@onready var temp=$MovPez
-@onready var yMax=int($ColorRect/MarginContainer/ColorRect.size.y)
-@onready var pez=$ColorRect/MarginContainer/ColorRect/TextureRect
-@onready var catch=$ColorRect/MarginContainer/ColorRect/Catch
+@onready var temp = $MovPez
+@onready var yMax = int($ColorRect/MarginContainer/ColorRect.size.y)
+@onready var pez = $ColorRect/MarginContainer/ColorRect/TextureRect
+@onready var catch = $ColorRect/MarginContainer/ColorRect/Catch
+@onready var pBar = $"../Progreso/ColorRect/MarginContainer/ProgressBar"
 
-@export var catchPeso: float =100
-@export var tMin: float =0.6
-@export var tMax: float =5.0
-@export var pezVel: float =0.6
-@export var catchLevantar: float =250
+@export var catchPeso: float = 100
+@export var tMin: float = 0.6
+@export var tMax: float = 5.0
+@export var pezVel: float = 0.6
+@export var catchLevantar: float = 250
+@export var velCompletado: float = 0.1
 
-var posSalto: int =0
+
+var posSalto: int = 0
 
 func _ready() -> void:
 	await get_tree().process_frame  # Espera un frame para que los nodos tengan su tamaño correcto
@@ -18,18 +21,23 @@ func _ready() -> void:
 	nuevoSalto()
 
 func _process(delta: float) -> void:
-	pez.position.y= lerpf(pez.position.y,posSalto,pezVel)
+	pez.position.y = lerpf(pez.position.y, posSalto, pezVel)
 	
 	if Input.is_action_pressed("Pescar"):
-		catch.position.y-=catchLevantar*delta
-	if catch.position.y+catch.size.y<=yMax:
-		catch.position.y+=catchPeso*delta
-
+		catch.position.y -= catchLevantar * delta
+	if catch.position.y + catch.size.y <= yMax:
+		catch.position.y += catchPeso * delta
+		
+	# Usar get_global_rect() para obtener las áreas exactas en coordenadas globales
+	var pez_rect = pez.get_global_rect()
+	var catch_rect = catch.get_global_rect()
+	
+	if pez_rect.intersects(catch_rect):
+		pBar.value += velCompletado
 
 func _on_mov_pez_timeout() -> void:
 	nuevoSalto()
 	
 func nuevoSalto() -> void:
-	posSalto= randi()%yMax
-	print("Salto", posSalto)
-	temp.wait_time=randf_range(tMin,tMax)
+	posSalto = randi() % yMax
+	temp.wait_time = randf_range(tMin, tMax)
